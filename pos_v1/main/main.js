2c345523a5101b6d21b8d0e3821cfb316de3e8f1
crystalynliu <crystalynliu@163.com>
2016-02-25 13:38:05 +0800

    add function getCount to main.js

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,1 +1,28 @@
-//TODO: Please write code in this file.
+function getItems(Tags,allItems){
+	var products = [];
+	var barcodes = getCount(Tags);
+	return products;
+}
+
+function getCount(Tags) {
+	var barcodes=[];
+	for(var i = 0;i<Tags.length;i++){
+		var stringArr = Tags[i].split('-');
+		var tag = {'barcode':stringArr[0],'count':0};
+		if(stringArr.length>1){
+			tag.count = stringArr[1];
+		}
+		if(barcodes.length!=0){
+			if(barcodes[barcodes.length-1].barcode==tag.barcode){
+				barcodes[barcodes.length-1].count++;
+			}
+			else{
+				barcodes.push(tag);
+			}
+		}else{
+			barcodes.push(tag);
+		}
+		
+	}
+	return barcodes;
+}
