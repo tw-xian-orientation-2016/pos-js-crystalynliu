d8bdea4f9ac361d0875b8087dfcfaa2ea7bbf715
crystalynliu <crystalynliu@163.com>
2016-02-25 10:46:56 +0800

    test:add one test to task-test.js

--- pos_v1/test/task-test.js
+++ pos_v1/test/task-test.js
@@ -1,1 +1,29 @@
-task-test.js
+describe('Test the Items information', function() {
+  var allItems;
+  var inputs;
+
+  beforeEach(function() {
+    allItems = loadAllItems();
+    inputs = [
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000003-2',
+      'ITEM000005',
+      'ITEM000005',
+      'ITEM000005'
+    ];
+  });
+  var expectResult= [
+  {Item1:allItems[1],count:5},
+  {Item3:allItems[3],count:2},
+  {Item5:allItems[5],count:3}];
+
+  it('show the Items and its count', function() {
+    var result = getItems(inputs,allItems);
+    expect(result).toEqual(expectResult);
+  });
+
+});
