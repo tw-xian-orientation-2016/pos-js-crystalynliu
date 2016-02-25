75bc5147d5e5e4c4093aaa3fce972fb1bb629e85
crystalynliu <crystalynliu@163.com>
2016-02-25 17:52:24 +0800

    add a new test of Test the  printReceipt

--- pos_v1/test/task-test.js
+++ pos_v1/test/task-test.js
@@ -136,4 +136,46 @@ describe('Test the  getReceipt', function() {
 
 });
 
+describe('Test the  printReceipt', function() {
+  var inputs;
+
+  beforeEach(function() {
+    inputs = {'CartItems':[
+        {product: {Item: {
+            barcode: 'ITEM000001',
+            name: '雪碧',
+            unit: '瓶',
+            price: 3.00
+          },count:5},total:12.00,save:3.00},
+  {product: {Item:{
+            barcode: 'ITEM000003',
+            name: '荔枝',
+            unit: '斤',
+            price: 15.00
+          },count:2},total:30.00,save:0.00},
+  {product: {Item:{
+            barcode: 'ITEM000005',
+            name: '方便面',
+            unit: '袋',
+            price: 4.50
+          },count:3},total:9.00,save:4.50}
+      ],'totalPrice':51.00,'totalSave':7.50};
+  });
+
+var expectText =
+      '***<没钱赚商店>收据***\n' +
+      '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
+      '名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n' +
+      '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n' +
+      '----------------------\n' +
+      '总计：51.00(元)\n' +
+      '节省：7.50(元)\n' +
+      '**********************';
+  it('show the Receipt', function() {
+    var result = getReceipt(inputs);
+    expect(result).toEqual(expectText);
+  });
+
+});
+
 
