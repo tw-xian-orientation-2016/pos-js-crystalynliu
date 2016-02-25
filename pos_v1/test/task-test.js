da5f843bb5977767d143f2780e09d4123b06d9fc
crystalynliu <crystalynliu@163.com>
2016-02-25 11:19:46 +0800

    modify task-test.js bug

--- pos_v1/test/task-test.js
+++ pos_v1/test/task-test.js
@@ -17,9 +17,9 @@ describe('Test the Items information', function() {
     ];
   });
   var expectResult= [
-  {Item1:allItems[1],count:5},
-  {Item3:allItems[3],count:2},
-  {Item5:allItems[5],count:3}];
+  {Item:allItems[1],count:5},
+  {Item:allItems[3],count:2},
+  {Item:allItems[5],count:3}];
 
   it('show the Items and its count', function() {
     var result = getItems(inputs,allItems);
