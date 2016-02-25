a394ea93cf33be7b0af83c655c170e298b305311
crystalynliu <crystalynliu@163.com>
2016-02-25 11:31:34 +0800

    modify the bug of task-test.js

--- pos_v1/test/task-test.js
+++ pos_v1/test/task-test.js
@@ -17,9 +17,25 @@ describe('Test the Items information', function() {
     ];
   });
   var expectResult= [
-  {Item:allItems[1],count:5},
-  {Item:allItems[3],count:2},
-  {Item:allItems[5],count:3}];
+  {Item: {
+      barcode: 'ITEM000001',
+      name: '雪碧',
+      unit: '瓶',
+      price: 3.00
+    },count:5},
+  {Item:{
+      barcode: 'ITEM000003',
+      name: '荔枝',
+      unit: '斤',
+      price: 15.00
+    },count:2},
+  {Item:{
+      barcode: 'ITEM000005',
+      name: '方便面',
+      unit: '袋',
+      price: 4.50
+    },count:3}
+  ];
 
   it('show the Items and its count', function() {
     var result = getItems(inputs,allItems);
